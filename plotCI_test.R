14c287fb80fa9435d701758634077a7d3cff7a0a
ivanhanigan <ivan.hanigan@gmail.com>
2025-09-17 06:26:49 +0800

    add test

---
+++ plotCI_test.R
@@ -0,0 +1,43 @@
+## TEST
+dat <- data.frame(x = 1:5, y = 10 * exp(0.3 * 1:5) + rnorm(5, 0, 2), uiw = 10 * exp(0.3 * 1:5) + rnorm(5, 0, 2) + (0.1 * (10 * exp(0.3 * 1:5) + rnorm(5, 0, 2)) + 1), liw = 10 * exp(0.3 * 1:5) + rnorm(5, 0, 2) - (0.1 * (10 * exp(0.3 * 1:5) + rnorm(5, 0, 2)) + 1))
+uiw <- dat$uiw
+source('plotCI.R')
+plotCI(x = dat, uiw = dat$uiw)
+
+## REFERENCE:
+## Original https://stat.ethz.ch/pipermail/r-help/1997-September/001758.html
+## Plot with error bars
+## Bill Venables wvenable at attunga.stats.adelaide.edu.au
+## Sat Sep 20 02:03:37 CEST 1997
+
+## The arguments are pretty self-explanatory, but here is a brief
+## run-down:
+
+## x, y are as you would expect, although both may be replaced by an
+##      xy-list, or if y is omitted, x is plotted against 1:length(x),
+
+## uiw is the distance(s) from y to the upper end of the interval,
+## liw is the distance(s) from y to the lower end of the interval,
+##      (if omitted taken to be the same as uiw),
+
+## sfrac is the fraction of the x-axis width to be used as the
+##      half-length of the seriphs that customarily go at each end
+##      of the intervals, (by default 1%).
+
+## NOTE: I simplified the original to remove the options
+## ... allows extra arguments to be passed to plot, eg xlab, ylab,
+##     type, lty, lwd, &c. (but not passed to segments()),
+
+## -- 
+## Bill Venables, Head, Dept of Statistics,    Tel.: +61 8 8303 5418
+## University of Adelaide,                     Fax.: +61 8 8303 3696
+## South AUSTRALIA.     5005.   Email: Bill.Venables at adelaide.edu.au
+
+## use par("usr") to get the range of the x and y axes in the plot.
+## NOT RUN
+y <- c(0,2,4)
+uiw <- c(0,1,1)
+plotCI(x=y,uiw= uiw)
+# plotCI(x=y,uiw= uiw, pch = 16, cex = 1.9)
+d <- plotCI(x= c(1,5,6), y = y, uiw= uiw)
+d
