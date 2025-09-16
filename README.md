28236dbf06914300a1a11dd9c78ff5759c8e2158
ivanhanigan <ivan.hanigan@gmail.com>
2025-09-17 06:12:15 +0800

    set up ex2

--- README.md
+++
@@ -1,3 +0,0 @@
-# air-health-sws-r-targets-technique-tweaking-tinkering
-air-health-sws-r-targets-technique-tweaking-tinkering
-# Ivan Hanigan & Anh Han
--- example_03_more_advanced_targets/R/script_model.tsd.R
+++
@@ -1,8 +0,0 @@
-
-##fit TSD using time series log-linear model using indicator variables for strata
-##without over-dispersion
-model.tsd=function(data)
-{
-  fit.tsd=glm(death~pm10tmean+as.factor(dowmonyr)+ns(tmpd,3)+ns(dptp,3),data=data,family=poisson)
-  return(fit.tsd)
-}
